55f4ddde839b3c6b349c962ce8a18e5e275dbcbb
Milan <jakubeclp@gmail.com>
2021-08-05 12:45:23 +0200

    Project Cars

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarsController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Factory|Application|View
+     */
+    public function index() {
+        $cars = Car::all();
+
+        return view('cars.index', [
+            'cars' => $cars
+        ]);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View|\Illuminate\Http\Response
+     */
+    public function create() {
+        return view('cars.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request) {
+        /**$car = new Car;
+        *$car->name = $request->input('name');
+        $car->founded = $request->input('founded');
+        $car->description = $request->input('description');**/
+
+        $car = Car::create([
+            'name'=> $request->input('name'),
+            'founded' => $request->input('founded'),
+            'description' => $request->input('description')
+        ]);
+        return redirect('/cars');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @param int $id
+     * @return Application|Factory|View|\Illuminate\Http\Response
+ *
+* public function show($id) {
+        * //
+    * }
+ *
+* /**
+     * Show the form for editing the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+
+        $car = Car::find($id)->first();
+        return view('cars.edit')->with('car', $car);
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id) {
+
+        $car = Car::where('id', $id)->update([
+            'name'=> $request->input('name'),
+            'founded' => $request->input('founded'),
+            'description' => $request->input('description')
+        ]);
+        return redirect('/cars');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function destroy(Car $car) {
+
+        $car->delete();
+        return redirect('/cars');
+
+    }
+}
---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+            <h1 class="text-5xl uppercase bold">
+                Cars
+            </h1>
+        </div>
+
+        <div class="pt-10">
+            <a href="cars/create"
+               class="border-b-2 pb-2 border-dotted italic text-gray-500">
+                Add a new car
+            </a>
+        </div>
+
+
+        <div class="w-5/6 py-10">
+            @foreach($cars as $car)
+                <div class="m-auto">
+
+                    <div class="float-right">
+                        <a class="border-b-2 pb-2 border-dotted italic text-green-500" href="cars/{{$car->id}}/edit">Edit</a>
+                    </div>
+
+                    <form action="/cars/{{$car->id}}" method="POST" class="float-right">
+                        @csrf
+                        @method('delete')
+                        <button class="border-b-2 pb-2 pt-7 pl-5 border-dotted italic text-red-500"
+                                type="submit">
+                            Delete &rarr;
+                        </button>
+                    </form>
+
+                    <span class="uppercase text-blue-500 font-bold text-xs italic">
+                    Founded: {{$car->founded}}
+                    </span>
+
+                    <h2 class="text-gray-700 text-5xl">
+                        {{$car->name}}
+                    </h2>
+
+                    <p class="text-lg text-gray-700 py-6">
+                        {{$car->description}}
+                    </p>
+
+                    <hr class="mt-4 mb-8">
+                </div>
+
+            @endforeach
+        </div>
+    </div>
+
+
+@endsection
